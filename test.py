d0557d533ed34cfa2f31b8a7ea08d9a983d6521a
prynar <84143917+prynar@users.noreply.github.com>
2021-06-11 12:34:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from bitcoinutils.keys import P2wpkhAddress, P2wshAddress, P2shAddress, PrivateK
 from bitcoinutils import constants
 import base58
 import hashlib
+import random
+def randomka():
+    d=random.randint(1157,1157)
+    return hex(d)
 def priv2WIF(h, compressed = False, testnet = False):
     #uncompressed: 0x80 + [32-byte secret] + [4 bytes of Hash() of previous 33 bytes], base58 encoded
     #compressed: 0x80 + [32-byte secret] + 0x01 + [4 bytes of Hash() previous 34 bytes], base58 encoded
@@ -18,9 +22,8 @@ def main():
     constants.NETWORK_SEGWIT_PREFIXES["mainnet"] = "sugar"
     # always remember to setup the network
     setup('mainnet')
-    kavo=priv2WIF(b'KxRMt7KypfEsLNSikhxTPYepDMBizHNmH5Bii3wssgesxrkHNJg6')
-    print(kavo)
-    private = PrivateKey.from_wif('KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn')
+    kavo=priv2WIF(randomka().encode('utf-8'))
+    private = PrivateKey.from_wif(kavo)
     public = private.get_public_key()
     segwit = public.get_segwit_address()
     address = segwit.to_string()
